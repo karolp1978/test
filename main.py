5212922bab18406f3a6e3e39c59bdcfb02ce487d
Karol <karolp1978@yahoo.com>
2021-04-15 16:17:47 +0200

    added ver 1.4

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     print("Hello again")
     print("new line 1.0")
     print("new line 1.2")
-    pintt("new line 1.3")
+    print("new line 1.3")
+    print("new line 1.4")
 if __name__ == "__main__":
     main()
